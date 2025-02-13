850edf0a35e949929bd7b92f0fe767a041bee100
3Vvv <333vovk@mail.ru>
2025-02-13 18:28:06 +0300

    Initial Commit

---
+++ VisionOSOtusDZ/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  VisionOSOtusDZ
+//
+//  Created by Вячеслав Вовк on 13.02.2025.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ VisionOSOtusDZ/VisionOSOtusDZApp.swift
@@ -0,0 +1,33 @@
+//
+//  VisionOSOtusDZApp.swift
+//  VisionOSOtusDZ
+//
+//  Created by Вячеслав Вовк on 13.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct VisionOSOtusDZApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
